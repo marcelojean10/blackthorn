c8b1a1ff34306f43fb80db594d6089024793dc1d
Marcelo Jean <marcelo.jean@flapper.aero>
2022-02-17 19:19:21 -0300

    refact(get-cart-by-user-id module): change call usecase

--- src/infra/nestjs/modules/features/get-cart-by-user-id.module.ts
+++ src/infra/nestjs/modules/features/get-cart-by-user-id.module.ts
@@ -2,7 +2,7 @@ import { PrismaConnectorModule } from '../db-connection/prisma/prisma-connector.
 import { NestModuleMetadataGenerator } from '../../../singletons/nest-module-metadata.generator';
 import { Module } from '@nestjs/common';
 import { GetCartByUserIdStrategy } from 'src/core/strategies/get-cart-by-user-id.strategy';
-import { GetCartByUserIdUsecase } from 'src/core/usecases/get-cart-by-user-id.protocol';
+import { GetCartByUserIdUsecase } from 'src/core/usecases/get-cart-by-user-id.usecase'
 import { GetCartByUserIdProtocol } from 'src/core/protocols/get-cart-by-user-id.protocol';
 import { GetCartByUserIdConnector } from 'src/adapter/connectors/get-cart-by-user-id.connector';
 
